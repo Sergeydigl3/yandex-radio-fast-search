469eba6c0847367216cac804312285b7fd86c18b
Dan <noreply@github.com>
2018-10-26 23:58:09 +0300

    hot_fix

--- popup.js
+++ popup.js
@@ -18,12 +18,12 @@ const yaLink = 'https://music.yandex.ru/search?text={0}';
 const yaName = 'Yandex.Music';
 
 function setPreset(value) {
-    console.log(localStorage['default_link']);
     localStorage['default_link'] = value;
+    console.log(localStorage['default_link']);
 }
 function setName(value) {
-    console.log(localStorage['default_name']);
     localStorage['default_name'] = value;
+    console.log(localStorage['default_name']);
 }
 document.getElementById('vk').addEventListener("click", function (){setName(vkName); setPreset(vkLink);});
 document.getElementById('sc').addEventListener("click", function (){setName(scName); setPreset(scLink);});
