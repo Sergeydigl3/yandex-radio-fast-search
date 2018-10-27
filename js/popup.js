d865b67c5ba58dfa5d16f37482ec8f01cda7e323
Serg <Sergeydigl3@gmail.com>
2018-10-27 15:41:35 +0300

    Text on found button fixed

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,13 @@
 function showHide() {
     console.log(document.getElementById('create_sets').style.display);
-    if (document.getElementById('create_sets').style.display=="none") {
-        document.getElementById('create_sets').style.display="block";
-    }else {
-        document.getElementById('create_sets').style.display="none";
+    if (document.getElementById('create_sets').style.display == "none") {
+        document.getElementById('create_sets').style.display = "block";
+    } else {
+        document.getElementById('create_sets').style.display = "none";
     }
 }
 
-document.getElementById('create_sets').style.display="none";
+document.getElementById('create_sets').style.display = "none";
 document.getElementById('new_sets').addEventListener("click", showHide);
 
 const vkLink = 'https://vk.com/audios318245880?q={0}';
@@ -19,14 +19,24 @@ const yaName = 'Yandex.Music';
 
 function setPreset(value) {
     chrome.storage.sync.set({"default_link": value});
-    // localStorage['default_link'] = value;
     console.log(localStorage['default_link']);
 }
+
 function setName(value) {
     chrome.storage.sync.set({"default_name": value});
-    // localStorage['default_name'] = value;
     console.log(localStorage['default_name']);
 }
-document.getElementById('vk').addEventListener("click", function (){setName(vkName); setPreset(vkLink);});
-document.getElementById('sc').addEventListener("click", function (){setName(scName); setPreset(scLink);});
-document.getElementById('ya').addEventListener("click", function (){setName(yaName); setPreset(yaLink);});
+
+// On click
+document.getElementById('vk').addEventListener("click", function () {
+    setName(vkName);
+    setPreset(vkLink);
+});
+document.getElementById('sc').addEventListener("click", function () {
+    setName(scName);
+    setPreset(scLink);
+});
+document.getElementById('ya').addEventListener("click", function () {
+    setName(yaName);
+    setPreset(yaLink);
+});
--- js/prog.js
+++ js/prog.js
@@ -14,10 +14,10 @@ chrome.storage.sync.get(['default_name'], function(result){
     localStorage['temp_name'] = result.default_name
 });
 
-if (localStorage['default_link'] == null) localStorage['default_link'] = 'https://soundcloud.com/search?q={0}';
-if (localStorage['default_name'] == null) localStorage['default_name'] = 'SoundCloud';
+if (localStorage['temp_link'] == null) localStorage['temp_link'] = 'https://soundcloud.com/search?q={0}';
+if (localStorage['temp_name'] == null) localStorage['temp_name'] = 'SoundCloud';
 
-$(".head__right").prepend("<a id=\"getsearch\" class=\"getsearch\">Найти в " + localStorage['default_name'] + "</a>");
+$(".head__right").prepend("<a id=\"getsearch\" class=\"getsearch\">Найти в " + localStorage['temp_name'] + "</a>");
 $(".head__right").prepend("<style>.getsearch{padding: 0 1vh;text-decoration:none;cursor: default;outline:none;display:inline-block;height:4vh;line-height:4vh;border-radius:4.1vh;margin:1vh 1.9vh;font-family:\'Montserrat\',sans-serif;font-size:1.25vh;text-transform:uppercase;text-align:center;letter-spacing:0.27vh;font-weight:600;color:#524f4e;background:white;box-shadow:0 0.74vh 1.38vh rgba(0,0,0,.1);transition:.3s}.getsearch:hover{background:#ffdb4d;box-shadow:0 1.38vh 1.9vh rgba(255, 219, 77,.4);color:#000;transform:translateY(-0.64vh)}</style>");
 document.getElementById('getsearch').addEventListener("click", event_link_start);
 
