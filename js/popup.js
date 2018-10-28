7a4002e62a52896140311f84330fe2f1008adef1
Serg <Sergeydigl3@gmail.com>
2018-10-28 11:44:16 +0300

    beautify input; blur and focus

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 function showHide() {
     console.log(document.getElementById('create_sets').style.display);
-    if (document.getElementById('create_sets').style.display == "none") {
+    if (document.getElementById('create_sets').style.display === "none") {
         document.getElementById('create_sets').style.display = "block";
         document.getElementById('new_sets').style.background = "#ffdb4d";
     } else {
@@ -30,6 +30,14 @@ const scName = 'SoundCloud';
 const yaLink = 'https://music.yandex.ru/search?text={0}';
 const yaName = 'Yandex.Music';
 
+function set_input_error(text) {
+    document.getElementById('error_text').innerHTML = text
+}
+
+function set_input_border(border) {
+    document.getElementById('set_link').style.border = border;
+}
+
 function setPreset(value) {
     chrome.storage.sync.set({"default_link": value});
     console.log(localStorage['default_link']);
@@ -38,6 +46,12 @@ function setPreset(value) {
 function setName(value) {
     chrome.storage.sync.set({"default_name": value});
     console.log(localStorage['default_name']);
+}
+
+function check_rules(input) {
+    if (!(input.startsWith('http://') || input.startsWith('https://'))) return 1;
+    if (!(input.includes('{0}'))) return 2;
+    return 3;
 }
 
 // On click
@@ -53,14 +67,59 @@ document.getElementById('ya').addEventListener("click", function () {
     setName(yaName);
     setPreset(yaLink);
 });
-var temp_link;
-document.getElementById('save_btn').addEventListener('click', function () {
-    if (document.getElementById('set_link').value == "") {
-        document.getElementById('set_link').style.border = "3px solid #ff3432";
+
+const input = document.getElementById('set_link');
+input.addEventListener("input", function () {
+    if (input.value === "") {
+        set_input_border("3px solid #ffdb4d")
     }
     else {
-        temp_link = document.getElementById('set_link').value;
-        document.getElementById('set_link').style.border = "3px solid #228b22";
-        console.log(temp_link);
+        const result = check_rules(input.value);
+        switch (result) {
+            case 1:
+                set_input_border("3px solid #ff3432");
+                set_input_error('Строка не начинается с http:// или https://');
+                break;
+
+            case 2:
+                set_input_border("3px solid #ff3432");
+                set_input_error('Строка не содержит {0}');
+                break;
+
+            case 3:
+                set_input_border("3px solid #228b22");
+                set_input_error('');
+                break;
+        }
+        console.log(result);
+    }
+});
+input.addEventListener("blur", function () {
+    const temp_border = input.style.border;
+    switch (temp_border) {
+        case "3px solid rgb(255, 219, 77)": //yellow
+            input.style.border = "1px dashed rgb(255, 219, 77)";
+            break;
+        case "3px solid rgb(255, 52, 50)": //red
+            input.style.border = "1px dashed rgb(255, 52, 50)";
+            break;
+        case "3px solid rgb(34, 139, 34)": //green
+            input.style.border = "1px dashed rgb(34, 139, 34)";
+            break;
+    }
+});
+
+input.addEventListener("focus", function () {
+    const temp_border = input.style.border;
+    switch (temp_border) {
+        case "1px dashed rgb(255, 219, 77)": //yellow
+            input.style.border = "3px solid rgb(255, 219, 77)";
+            break;
+        case "1px dashed rgb(255, 52, 50)": //red
+            input.style.border = "3px solid rgb(255, 52, 50)";
+            break;
+        case "1px dashed rgb(34, 139, 34)": //green
+            input.style.border = "3px solid rgb(34, 139, 34)";
+            break;
     }
 });
